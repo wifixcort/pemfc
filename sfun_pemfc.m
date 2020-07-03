7ea7cf2178511112d4bb9b9773984b7302f50049
Ricardo Mena C <rgacort12@gmail.com>
2020-07-03 00:24:56 -0600

    Salida no linealizada y parámetros de entrada
    Signed-off-by: Ricardo Mena C <rgacort12@gmail.com>

--- sfun_pemfc.m
+++ sfun_pemfc.m
@@ -22,9 +22,21 @@ setup(block);
 %
 function setup(block)
 
+%De las ecuaciones 19 y 20 se tiene que:
+
+% x = [pH_2 pO_2 pH_2*O_C]^T %Presiones parciales de cada gas
+% u = [H_2in O_2in i]^T  %i es la intensidad de corriente
+% y = V
+
+% R = 8.3144 % Constantes de gas ideal
+% F = 96439 % Constante de Faraday
+% P_std = 101325 % Presión estándar
+
 % Se registra el número de puertos de entrada y salida
 block.NumInputPorts  = 2; % Cantidad de entradas
-block.NumOutputPorts = 2; % Cantidad de salidas
+block.NumOutputPorts = 1; % Cantidad de salidas
+% OJO ====>>> Inicialmente el sistema contempla una sola salida pero luego
+              % de la linealización son 3 salidas
 
 % Setup port properties to be inherited or dynamic
 block.SetPreCompInpPortInfoToDynamic;
@@ -60,7 +72,8 @@ block.NumDialogPrms     = 9; % en este caso hay oho parámetros de entrada
 
 block.SampleTimes = [0, 0]; % Tiempo de muestreo heredado
 
-block.NumContStates = 2; % Definicion de la cantidad de variables de estado
+block.NumContStates = 3; % Definicion de la cantidad de variables de estado
+% Inicialmente son 3, pero luego de linealizarlo son 5 OJO <====
   
 block.SimStateCompliance = 'DefaultSimState';
 % -----------------------------------------------------------------
@@ -74,16 +87,45 @@ block.RegBlockMethod('SetInputPortSamplingMode',@SetInputPortSamplingMode); % Ne
 
 function Inicializacion(block)
 
-block.ContStates.Data = block.DialogPrm(9).Data; % esto lo que hace es
+block.ContStates.Data = block.DialogPrm(13).Data; % esto lo que hace es
 %             tomar el valor del estado inicial como si fuera un parámetro
 %end Inicializacion
 
-%
+
 function Salidas(block)
 % Acá se escribe las ecuaciones de salida
+% y = V <= Sin aplicar linealización exacta, V es igual a la ecuación 20
+
+N  = block.DialogPrm(4).Data; % Número de celdas
+Eo = block.DialogPrm(7).Data; % Tensión sin carga
+T  = block.DialogPrm(6).Data; % Temperatura de operación
+
+R = 8.3144 % Constantes de gas ideal
+F = 96439 % Constante de Faraday
+P_std = 101325 % Presión estándar
+
+% x = [pH_2 pO_2 pH_2*O_C]^T %Presiones parciales de cada gases
 x = block.ContStates.Data; % el estado actual
-block.OutputPort(1).Data = 1; % En este caso, la salida 1
-block.OutputPort(2).Data = 1; % En este caso, la salida 2
+pH_2 = x(1);
+pO_2 = x(2);
+pH_2O = x(3);
+
+% u = [H_2in O_2in i]^T
+O = block.InputPort(2).Data; % Solamente se requiere la cantidad de oxígeno
+
+%Se requiere para deternimar L
+% i, i_n, i_o, i_l, r, a y b                                    <==========
+L = 0; %Aún no se conocen los parámetros necesarios
+%Determinar de donde salen todas estas corrientes               <==========
+% Esta condición en Cero crearía un sistema sin pérdidas
+
+V_1 = N*(Eo+((R*T)/(2*F))*ln((pH_2*sqrt(pO_2/P_std))/pH_2O)-L);
+
+block.OutputPort(1).Data = V_1; % Salida 1
+%Dos salidas extra del modelo linealizado
+%block.OutputPort(2).Data = x4 o V_2 % Salida 2
+%block.OutputPort(2).Data = x5 o V_3; % Salida 3
+
 %end Salidas
 
 function ModeloEstados(block)
@@ -93,16 +135,31 @@ Ac = block.DialogPrm(1).Data; % Área activa
 Va = block.DialogPrm(2).Data; % Volumen del anodo
 Vc = block.DialogPrm(3).Data; % Volumen del cátodo
 N  = block.DialogPrm(4).Data; % Número de celdas
-Po = block.DialogPrm(5).Data; % Presion de operacion
+Po = block.DialogPrm(5).Data; % Presion de operación
 T  = block.DialogPrm(6).Data; % Temperatura de operación
 Eo = block.DialogPrm(7).Data; % Tensión sin carga
 Fu = block.DialogPrm(8).Data; % Factor de utilización
+Ref = block.DialogPrm(9).Data; % Constante de reformador
+Fc = block.DialogPrm(10).Data; % Factor de conversión
+Met_sig = block.DialogPrm(11).Data; % Referencia de señal de metano 0.000015
+HO_fr = block.DialogPrm(12).Data; % Radio de flujo Hidrógeno-Oxígeno
 
-x = block.ContStates.Data; % el valor del estado actual
-u1 =  block.InputPort(1).Data; % el valor de la entrada 1 actual
-u2 =  block.InputPort(2).Data; % el valor de la entrada 1 actual
+% x = [pH_2 pO_2 pH_2O_C]^T %Presiones parciales de cada gas
+x = block.ContStates.Data; % Vector de estados iniciales
+pH_2 = x(1);
+pO_2 = x(2);
+pH_2O_C = x(3);
 
-block.Derivatives.Data = [dx1dt;dx2dt]; % actualizacion del bloque de la S-function
+% u = [H_2in O_2in i]^T
+H_2in =  block.InputPort(1).Data; % Hidrógeno de entrada
+O_2in =  block.InputPort(2).Data; % Oxígeno de entrada
+i =  block.InputPort(2).Data; % i es la intensidad de corriente
+
+%=====Desarrollar las matrices de estado aquí=====
+%=================================================
+
+
+block.Derivatives.Data = [dx1dt;dx2dt;dx3dt]; % actualizacion del bloque de la S-function
 %end ModeloEstados
 
 function SetInputPortSamplingMode(s, port, mode)
