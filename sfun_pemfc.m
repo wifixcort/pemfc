81f08030a27dca64b1c883c883c35e78c4d385de
Ricardo Mena C <rgacort12@gmail.com>
2020-07-03 12:10:52 -0600

    Modelo no lineal(no revisado)
    Signed-off-by: Ricardo Mena C <rgacort12@gmail.com>

--- sfun_pemfc.m
+++ sfun_pemfc.m
@@ -33,7 +33,7 @@ function setup(block)
 % P_std = 101325 % Presión estándar
 
 % Se registra el número de puertos de entrada y salida
-block.NumInputPorts  = 2; % Cantidad de entradas
+block.NumInputPorts  = 3; % Cantidad de entradas
 block.NumOutputPorts = 1; % Cantidad de salidas
 % OJO ====>>> Inicialmente el sistema contempla una sola salida pero luego
               % de la linealización son 3 salidas
@@ -54,27 +54,38 @@ block.InputPort(2).DatatypeID  = 0;  % double
 block.InputPort(2).Complexity  = 'Real';
 block.InputPort(2).DirectFeedthrough = true;
 
+% Propiedades de los puertos de entrada
+block.InputPort(3).Dimensions        = 1;
+block.InputPort(3).DatatypeID  = 0;  % double
+block.InputPort(3).Complexity  = 'Real';
+block.InputPort(3).DirectFeedthrough = true;
+
 % Propiedades de los puertos de salida
 block.OutputPort(1).Dimensions       = 1;
 block.OutputPort(1).DatatypeID  = 0; % double
 block.OutputPort(1).Complexity  = 'Real';
 block.OutputPort(1).SamplingMode = 'Sample';
 
-block.OutputPort(2).Dimensions       = 1;
-block.OutputPort(2).DatatypeID  = 0; % double
-block.OutputPort(2).Complexity  = 'Real';
-block.OutputPort(2).SamplingMode = 'Sample';
+%block.OutputPort(2).Dimensions       = 1;
+%block.OutputPort(2).DatatypeID  = 0; % double
+%block.OutputPort(2).Complexity  = 'Real';
+%block.OutputPort(2).SamplingMode = 'Sample';
+
+%block.OutputPort(3).Dimensions       = 1;
+%block.OutputPort(3).DatatypeID  = 0; % double
+%block.OutputPort(3).Complexity  = 'Real';
+%block.OutputPort(3).SamplingMode = 'Sample';
 
 % Número de parámetros
-block.NumDialogPrms     = 9; % en este caso hay oho parámetros de entrada
-                             % el noveno "parámentro" corresponde al vector
+block.NumDialogPrms     = 13; % en este caso hay 12 parámetros de entrada
+                             % el 13 "parámentro" corresponde al vector
                              % de entrada
 
 block.SampleTimes = [0, 0]; % Tiempo de muestreo heredado
 
 block.NumContStates = 3; % Definicion de la cantidad de variables de estado
-% Inicialmente son 3, pero luego de linealizarlo son 5 OJO <====
-  
+% OJO ===>>>    Inicialmente son 3, pero luego de linealizarlo son 5
+
 block.SimStateCompliance = 'DefaultSimState';
 % -----------------------------------------------------------------
 % Ahora se registran los métodos internos de la S-function
@@ -97,12 +108,12 @@ function Salidas(block)
 % y = V <= Sin aplicar linealización exacta, V es igual a la ecuación 20
 
 N  = block.DialogPrm(4).Data; % Número de celdas
-Eo = block.DialogPrm(7).Data; % Tensión sin carga
 T  = block.DialogPrm(6).Data; % Temperatura de operación
+Eo = block.DialogPrm(7).Data; % Tensión sin carga
 
-R = 8.3144 % Constantes de gas ideal
-F = 96439 % Constante de Faraday
-P_std = 101325 % Presión estándar
+R = 8.3144; % Constantes de gas ideal
+F = 96439; % Constante de Faraday
+P_std = 101325; % Presión estándar
 
 % x = [pH_2 pO_2 pH_2*O_C]^T %Presiones parciales de cada gases
 x = block.ContStates.Data; % el estado actual
@@ -119,7 +130,7 @@ L = 0; %Aún no se conocen los parámetros necesarios
 %Determinar de donde salen todas estas corrientes               <==========
 % Esta condición en Cero crearía un sistema sin pérdidas
 
-V_1 = N*(Eo+((R*T)/(2*F))*ln((pH_2*sqrt(pO_2/P_std))/pH_2O)-L);
+V_1 = N*(Eo+((R*T)/(2*F))*log((pH_2*sqrt(pO_2/P_std))/pH_2O)-L);
 
 block.OutputPort(1).Data = V_1; % Salida 1
 %Dos salidas extra del modelo linealizado
@@ -137,25 +148,44 @@ Vc = block.DialogPrm(3).Data; % Volumen del cátodo
 N  = block.DialogPrm(4).Data; % Número de celdas
 Po = block.DialogPrm(5).Data; % Presion de operación
 T  = block.DialogPrm(6).Data; % Temperatura de operación
-Eo = block.DialogPrm(7).Data; % Tensión sin carga
-Fu = block.DialogPrm(8).Data; % Factor de utilización
-Ref = block.DialogPrm(9).Data; % Constante de reformador
-Fc = block.DialogPrm(10).Data; % Factor de conversión
-Met_sig = block.DialogPrm(11).Data; % Referencia de señal de metano 0.000015
-HO_fr = block.DialogPrm(12).Data; % Radio de flujo Hidrógeno-Oxígeno
+%Eo = block.DialogPrm(7).Data; % Tensión sin carga
+%Fu = block.DialogPrm(8).Data; % Factor de utilización
+%Ref = block.DialogPrm(9).Data; % Constante de reformador
+%Fc = block.DialogPrm(10).Data; % Factor de conversión
+%Met_sig = block.DialogPrm(11).Data; % Referencia de señal de metano 0.000015
+%HO_fr = block.DialogPrm(12).Data; % Radio de flujo Hidrógeno-Oxígeno
 
 % x = [pH_2 pO_2 pH_2O_C]^T %Presiones parciales de cada gas
 x = block.ContStates.Data; % Vector de estados iniciales
-pH_2 = x(1);
-pO_2 = x(2);
-pH_2O_C = x(3);
+%pH_2 = x(1);
+%pO_2 = x(2);
+%pH_2O_C = x(3);
 
 % u = [H_2in O_2in i]^T
 H_2in =  block.InputPort(1).Data; % Hidrógeno de entrada
 O_2in =  block.InputPort(2).Data; % Oxígeno de entrada
-i =  block.InputPort(2).Data; % i es la intensidad de corriente
+i =  block.InputPort(3).Data; % i es la intensidad de corriente
+
+R = 8.3144; % Constantes de gas ideal
+F = 96439; % Constante de Faraday
+
+K_r = N/(4*F); %
+
+M_1_1 = ((R*T)/Va)*(1-(x(1)/Po));
+M_2_2 = ((R*T)/Vc)*(1-(x(2)/Po));
+M_2_3 = -((R*T)/(Va*Po))*x(3);
+
+RT = (R*T)/(Vc*Po);
+
+M_3_1 = RT*(-2*K_r*Ac+2*K_r*Ac*x(1));
+M_3_2 = RT*(-K_r*Ac+2*K_r*Ac*x(2));
+M_3_3 = RT*(2*K_r*Ac-2*K_r*Ac*x(3));
 
 %=====Desarrollar las matrices de estado aquí=====
+dx1dt = M_1_1*H_2in+M_3_1*i;
+dx2dt = M_2_2*O_2in+M_3_2*i;
+dx3dt = M_2_3*O_2in+M_3_3*i;
+
 %=================================================
 
 
